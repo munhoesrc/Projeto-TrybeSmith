0c12473c1109a6998cb0deb675a5cda5402dbe0a
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-02-07 14:48:35 -0300

    feat: 04-list-orders

--- src/models/OrderModel.ts
+++ src/models/OrderModel.ts
@@ -8,16 +8,14 @@ export default class OrderModel {
     this.connection = connection;
   }
 
-  // A função SQL / JSON JSON_ARRAYAGG é uma função agregada.
-  // Ele pega como entrada uma coluna de expressões SQL,
-  // converte cada expressão em um valor JSON e retorna uma única matriz JSON que contém esses valores JSON.
-  public getAll = async (): Promise<IOrder[]> => {
+  // Recupera todos os pedidos do banco de dados.
+  public async getAll(): Promise<IOrder[]> {
     const query = `SELECT orders.id, orders.user_id as userId,
-    JSON_ARRAYAGG(products.id) as productsIds
-    FROM Trybesmith.orders JOIN Trybesmith.products
-    ON ProductService.order_id = orders.id GROUP BY orders.id`;
+      JSON_ARRAYAGG(products.id) as productsIds
+      FROM Trybesmith.orders JOIN Trybesmith.products
+      ON products.order_id = orders.id GROUP BY orders.id`;
 
-    const [db] = await this.connection.execute(query);
-    return db as IOrder[];
-  };
+    const [orders] = await this.connection.execute(query);
+    return orders as IOrder[];
+  }
 }
